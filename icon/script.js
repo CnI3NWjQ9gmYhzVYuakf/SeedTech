f8307b4cf786e51aa0dff5b45c04755e4e8870c9
CnI3NWjQ9gmYhzVYuakf <86827614+CnI3NWjQ9gmYhzVYuakf@users.noreply.github.com>
2021-09-12 22:42:22 +0100

    Update script.js

--- icon/script.js
+++ icon/script.js
@@ -1,11 +1,6 @@
 var canvas, context, hw, aw, ah, map, ow, col, url;
 window.charmap;
 window.numToGen;
-  
-
-
-
-
 function plot(y, x) //Treat grid as a 5x5
 {
     x = Math.floor(x * s);
@@ -15,16 +10,10 @@ function plot(y, x) //Treat grid as a 5x5
     context.rect(x, y, s, s);
     context.fillStyle = pickColor();
     context.fill();
-
 }
-
 function erase(){
   context.clearRect(0,0,aw, ah);
-
 }
-
-
-
 function updateListen(){
       ow = hw;
       hw = hex_md5($('#gen').val());
@@ -32,48 +21,15 @@ function updateListen(){
         return;
       massPlot();
 }
-
 function pickColor(){
-  var r = "7";
-  var g = "7";
-  var b = "7";
-  var r1 = hw.substring(0, 1);
-  var r2 = hw.substring(1, 2);
-  var p1 = parseInt("0x" + r1, 16);
-  var p2 = parseInt("0x" + r2, 16);
-  if(p1 < 6)
-    r = r1;
-  else if(p1 < 12)
-    g = r1;
-  else
-    b = r1;
-  if(p2 < 6)
-    r = r2;
-  else if(p2 < 12)
-    g = r2;
-  else
-    b = r2;  
-  var col = "#"+r+g+b;
-  var br = (16 - parseInt(r, 16)).toString(16);
-  var bg = (16 - parseInt(g, 16)).toString(16);
-  var bb = (16 - parseInt(b, 16)).toString(16);
-  var bcol = "#"+br+bg+bb;
-  document.body.style.color = bcol;
-
-
+  var col = "#00ff00";
   return col;
-
-
 }
-
 function genRandom(){
-
     $('#gen').val(Math.random() * 10000);
     console.log("Last value: " + $('#gen').val());
     updateListen();
 }
-
-
 function massPlot(){
   erase();
   var dup = 0;
@@ -87,17 +43,12 @@ function massPlot(){
       if(parseInt("0x" + hw.substring(num * 2, num * 2+2), 16) % 2 == 1)
       {
         dup = 1 + (max - j);
-
         plot(i, j);
         plot(i, dup);
       }
     }
-
   }
-
 }
-
-
 $(function(){
     aw = ah = 480;
     s = aw / 5;
@@ -107,16 +58,12 @@ $(function(){
       for(var j = 0; j < map[0].length; j++)
         map[i][j] = 0;
     }
-
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
     context.strokeStyle = 'black';
     genRandom();
       hw = hex_md5($('#gen').val());
-
       ow = hw = "";    
       updateListen();
     $('#gen').change(function(){updateListen()}).click(function(){updateListen()}).keyup(function(){updateListen()});    
-
-
 });
